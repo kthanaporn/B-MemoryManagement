cdc5f3350ac8cc3d6cf40eaa011f4747a2d34861
Thanaporn Kitworakiat <129836331+kthanaporn@users.noreply.github.com>
2024-10-28 00:10:21 +0700

    Add files via upload

---
+++ memoryManagement.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int global_1;
+int global_2;
+int global_3;
+
+int function(int n){
+    if(n == 4)
+        return 0;
+    int stack_n = n;
+    printf("Address of stack_%d in function: %p\n", n, (void*)&stack_n);
+    function(n+1);
+}
+int main () {
+    printf("Address of global_1: %p\n", (void*)&global_1);
+    printf("Address of global_2: %p\n", (void*)&global_2);
+    printf("Address of global_3: %p\n", (void*)&global_3);
+
+    printf("\nCalling function to observe stack growth:\n");
+    function(1);
+
+    int *heap_1 = (int*)malloc(sizeof(int));
+    int *heap_2 = (int*)malloc(sizeof(int));
+    int *heap_3 = (int*)malloc(sizeof(int));
+
+    printf("\nAddress of heap_1: %p\n", (void*)heap_1);
+    printf("Address of heap_2: %p\n", (void*)heap_2);
+    printf("Address of heap_3: %p\n", (void*)heap_3);
+
+    free(heap_1);
+    free(heap_2);
+    free(heap_3);
+
+    return 0;
+    
+}
+
+
